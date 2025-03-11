144426a93081b5e7bff32781115ed2f8eb9d55c8
Junichiro Niimi <54612149+jniimi@users.noreply.github.com>
2025-03-11 13:41:25 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('README.md', 'r', encoding='utf-8') as fp:
 
 setup(
     name='clumpi',
-    version="0.0.1",
+    version="1.0.0",
     description="Calculate Clumpiness index by Zhang, Bradlow and Small (2015)",
     long_description=readme,
     long_description_content_type='text/markdown',
     author='jniimi',
     author_email='jniimi@meijo-u.ac.jp',
-    url='https://github.com/jniimi/clumpi',
+    url='https://jun-systems.info',
     download_url='https://github.com/jniimi/clumpi',
     packages=find_packages(),
     keywords='RFMC Clumpiness marketing CRM',
